e7778560b944987dbe2b5962c774d553686ffb00
Iguanya <georgezmwanchez1996@gmai.com>
2024-09-15 14:03:29 +0300

    Added chefs forms

---
+++ mychef/mychef/doctype/register_chef/test_register_chef.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, George Mwangi and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRegisterChef(FrappeTestCase):
+	pass
